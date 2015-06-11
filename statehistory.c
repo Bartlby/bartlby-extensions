52ba07509433a61d62e7ba1a672068ba0499fa23
Helmut Januschka <helmut@januschka.com>
2015-06-11 20:28:29 +0000

    adopt to new statehistory path structure

--- statehistory.c
+++ statehistory.c
@@ -103,9 +103,12 @@ int statehistory_check_will_run(struct service * svc) {
 	x=statehistory_find_shm_place_for_id(svc->service_id);
 	current_hash = state_hash_map[x].sha1_hash;
 	
+
+	diff=time(NULL)-state_hash_map[x].last_write;
+
 	if(strcmp(new_hash_str, current_hash) != 0 || diff >= 60*2) {
 		
-		diff=time(NULL)-state_hash_map[x].last_write;
+		
 		
 		if(diff >= 30) { //FIXME: min time
 			
